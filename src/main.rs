e7409f6a0b3d216e9c4cd096783668229769d0c8
Ben DiFrancesco <ben@scopelift.co>
2022-11-20 21:47:08 -0500

    Setup the program and memory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let str_program = ".";
+
+    let program: Vec<char> = str_program.chars().collect();
+    let counter = 0;
+
+    let memory: Vec<u32> = vec![0; 30000];
+    let pointer = 0;
+
+    if program[counter] == '.' {
+        print_memory(memory[pointer]);
+    }
+
+    print!("\n");
+}
+
+fn print_memory(mem: u32) {
+    unsafe {
+        let c: char = char::from_u32_unchecked(mem);
+        print!("{}", c);
+    }
 }
