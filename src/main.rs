d9c38840cf582638dbbc29d4b2a7403f7e2bf76f
Ben DiFrancesco <ben@scopelift.co>
2022-11-20 23:03:34 -0500

    Add loop support

--- src/main.rs
+++ src/main.rs
@@ -15,18 +15,45 @@ fn main() {
     // ";
 
     // print "HI" with back and forward movement
+    // let str_program = "
+    //     ++++++++++ ++++++++++ ++++++++++ ++++++++++ ++++++++++ ++++++++++ ++++++++++ +++
+    //     >
+    //     ++++++++++ ++++++++++ ++++++++++ ++++++++++ ++++++++++ ++++++++++ ++++++++++ ++.
+    //     < .
+    // ";
+
+    // Prints "54321"
+    // let str_program = "
+    //     ++++++++++ ++++++++++ ++++++++++ ++++++++++ ++++++++++ +++
+    //     > +++++
+    //     [<.->-]
+    // ";
+
+    // Prints "555444333222111" w/ nested loops
     let str_program = "
-        ++++++++++ ++++++++++ ++++++++++ ++++++++++ ++++++++++ ++++++++++ ++++++++++ +++
-        >
-        ++++++++++ ++++++++++ ++++++++++ ++++++++++ ++++++++++ ++++++++++ ++++++++++ ++.
-        < .
+        ++++++++++ ++++++++++ ++++++++++ ++++++++++ ++++++++++ +++
+        > +++++
+        [
+        > +++
+        [<<.>>-]
+        <<-
+        >-
+        ]
     ";
+
+    // Syntax errors
+    // let str_program = "]";
+    // let str_program = "++]";
+    // let str_program = "[]]";
+
 
     let program: Vec<char> = str_program.chars().collect();
     let mut counter = 0;
 
     let mut memory: Vec<u32> = vec![0; 30000];
     let mut pointer = 0;
+
+    let mut loop_starts: Vec<usize> = Vec::new();
 
     while counter < str_program.len() {
         if program[counter] == '.' {
@@ -39,6 +66,18 @@ fn main() {
             pointer += 1;
         } else if program[counter] == '<' {
             pointer -= 1;
+        } else if program[counter] == '[' {
+            loop_starts.push(counter);
+        } else if program[counter] == ']' {
+            let start = loop_starts
+                                    .last()
+                                    .expect("Syntax error: Unexpected closing bracket");
+
+            if memory[pointer] == 0 {
+                loop_starts.pop();
+            } else if memory[pointer] != 0 {
+                counter = *start;
+            }
         }
 
         counter += 1;
